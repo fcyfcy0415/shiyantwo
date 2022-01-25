16b1ad3bff852c52f8730ba7af99bf7288891d88
fcy <3177527495@qq.com>
2022-01-25 10:03:02 +0800

    fix: dist 内容提交

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,28 @@
 // vue.config.js
 const path = require('path');
 
-module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
+// module.exports = {
+//   configureWebpack: {
+//     resolve: {
+//       alias: {
+//         '@core': path.join(__dirname, 'core'),
+//       },
+//     },
+//   },
+// };
+module.exports = { 
+  publicPath: './', 
+  configureWebpack: { 
+    resolve: { 
+      alias: { 
+        'assets': '@/assets', 
+        'common': '@/common', 
+        'components': '@/components', 
+        'network': '@/network', 
+        'views': '@/views', 
+        'plugins': '@/plugins',
         '@core': path.join(__dirname, 'core'),
-      },
-    },
-  },
-};
+      }
+    }
+  }
+}
